9a76334802641b8f7997317bd7cf63a8dd09d70b
4E14 Chu Yat Hong <sy9660@syss.edu.hk>
2019-04-12 18:29:46 +0800

    Update Photo.py

--- photo_email/photo.py
+++ photo_email/photo.py
@@ -6,12 +6,15 @@ import cv2
 import numpy as np
 
 class Photo:
-    def __init__(self, path, name):
+    def __init__(self, name, path):
+        self.id = name
         self.path = path
-        self.name = name
-        self.encodings = [];
+        self.faces = []
+        self.process()
 
-    def print_name(self) :
-        print(self.name)
-
-    def 
+    def process(self):
+        img = fr.api.load_image_file(self.path)
+        encoding = fr.api.face_encodings(img)
+        location = fr.api.face_locations(img)
+        for i in range(len(encoding)) :
+            self.faces.append((location[i], encoding[i]))
