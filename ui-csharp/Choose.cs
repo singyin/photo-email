2e1ddb82f59f4d3614159c032e1a54ad143b703e
William Tseng <43490658+sy9711@users.noreply.github.com>
2019-05-23 23:05:03 +0800

    Add C# UI
    Finished basic structure of UI

---
+++ ui-csharp/Choose.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Blackhole
+{
+    public partial class Choose : Form
+    {
+        // For testing ***
+        const string st = "C:/Users/Administrator/source/repos/Project Blackhole/Project Blackhole/bin/Source Pic/";
+        public static string Path = "C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python.exe";
+        //***
+        List<string> Arr = new List<string>();
+        void Run()
+        {
+            //Provide script path
+            string Pyname = "C:/Users/Administrator/source/repos/Project Blackhole/Project Blackhole/bin/Testing Python/testing.py";
+            //Create Process Info
+            ProcessStartInfo StartInfo = new ProcessStartInfo(Path);
+            StartInfo.FileName = Path;
+            StartInfo.Arguments = $"\"{Pyname}\"";
+            //Config
+            StartInfo.UseShellExecute = false;
+            StartInfo.CreateNoWindow = true;
+            StartInfo.RedirectStandardError = true;
+            StartInfo.RedirectStandardOutput = true;
+            //Execution
+            string output = "";
+            string err = "";
+            using (Process pro = Process.Start(StartInfo))
+            {
+                err = pro.StandardError.ReadToEnd();
+                output = pro.StandardOutput.ReadToEnd();
+            }
+            string Buffer = "";
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (output[i] == '\n')
+                {
+                    Arr.Add(Buffer);
+                    Buffer = "";
+                }
+                else Buffer += output[i];
+            }
+        }
+        public Choose(string s)
+        {
+            InitializeComponent();
+            Run();
+        }
+        int cur = 0;
+        void Setbrowse(int from)
+        {
+            if (from < Arr.Count) pictureBox1.ImageLocation = Arr[from]; else pictureBox1.ImageLocation = st + "unavailable.png";
+            if (from+1 < Arr.Count) pictureBox2.ImageLocation = Arr[from+1]; else pictureBox2.ImageLocation = st + "unavailable.png";
+            if (from+2 < Arr.Count) pictureBox3.ImageLocation = Arr[from+2]; else pictureBox3.ImageLocation = st + "unavailable.png";
+            if (from+3 < Arr.Count) pictureBox4.ImageLocation = Arr[from+3]; else pictureBox4.ImageLocation = st + "unavailable.png";
+            if (from+4 < Arr.Count) pictureBox5.ImageLocation = Arr[from+4]; else pictureBox5.ImageLocation = st + "unavailable.png";
+            if (from+5 < Arr.Count) pictureBox6.ImageLocation = Arr[from+5]; else pictureBox6.ImageLocation = st + "unavailable.png";
+        }
+        private void Choose_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = pictureBox2.SizeMode = pictureBox3.SizeMode = pictureBox4.SizeMode = pictureBox5.SizeMode = pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            Setbrowse(0);
+            prev.Hide();
+            if (Arr.Count <= 6) next.Hide();
+        }
+
+        
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Prev_Click(object sender, EventArgs e)
+        {
+            next.Show();
+            if (--cur == 0) prev.Hide();
+            Setbrowse(cur * 6);
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            prev.Show();
+            if ((++cur+1)*6 >= Arr.Count) next.Hide();
+            Setbrowse(cur * 6);
+        }
+    }
+}
---
+++ ui-csharp/Login.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Project_Blackhole
+{
+    partial class main
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(419, 426);
+            this.textBox1.MaximumSize = new System.Drawing.Size(300, 300);
+            this.textBox1.MaxLength = 5;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 33);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Student ID";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(545, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Change Python Interpretor Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "\"C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python.exe\"";
+            this.label1.Click += new System.EventHandler(this.Label1_Click_1);
+            // 
+            // main
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project_Blackhole.Properties.Resources.Sing_Yin_Secondary_School__Choi_Wan__from_Choi_Hing_Road__19_5_2012;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Face Recognition System";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private EventHandler TextBox1_TextChanged;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
