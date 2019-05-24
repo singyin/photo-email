58c4170bc844bf4daf745da82bdf314349efc5e9
sy9711 <sy9711@syss.edu.hk>
2019-05-24 17:46:09 +0800

    Update folder structrue and UI

--- ui-csharp/Choose.Designer.cs
+++ ui-csharp/Choose.Designer.cs
@@ -38,12 +38,24 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
             this.SuspendLayout();
             // 
             // confirm_button
@@ -56,6 +68,7 @@
             this.confirm_button.TabIndex = 11;
             this.confirm_button.Text = "Confirm";
             this.confirm_button.UseVisualStyleBackColor = false;
+            this.confirm_button.Click += new System.EventHandler(this.Confirm_button_Click);
             // 
             // prev
             // 
@@ -149,6 +162,67 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
             // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox7.Location = new System.Drawing.Point(42, 91);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(197, 105);
+            this.pictureBox7.TabIndex = 21;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Visible = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox10.Location = new System.Drawing.Point(42, 284);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(197, 105);
+            this.pictureBox10.TabIndex = 22;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Visible = false;
+            this.pictureBox10.Click += new System.EventHandler(this.PictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox8.Location = new System.Drawing.Point(295, 91);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(197, 105);
+            this.pictureBox8.TabIndex = 23;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Visible = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox11.Location = new System.Drawing.Point(295, 284);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(197, 105);
+            this.pictureBox11.TabIndex = 24;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Visible = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox9.Location = new System.Drawing.Point(545, 91);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(197, 105);
+            this.pictureBox9.TabIndex = 25;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Visible = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox12.Location = new System.Drawing.Point(545, 284);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(197, 105);
+            this.pictureBox12.TabIndex = 26;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Visible = false;
+            // 
             // Choose
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -164,6 +238,12 @@
             this.Controls.Add(this.next);
             this.Controls.Add(this.prev);
             this.Controls.Add(this.confirm_button);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox12);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Choose";
@@ -175,6 +255,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -183,11 +269,17 @@
         private System.Windows.Forms.Button confirm_button;
         private System.Windows.Forms.Button prev;
         private System.Windows.Forms.Button next;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox12;
     }
 }
--- ui-csharp/Choose.cs
+++ ui-csharp/Choose.cs
@@ -14,18 +14,18 @@ namespace Project_Blackhole
 {
     public partial class Choose : Form
     {
-        // For testing ***
-        const string st = "C:/Users/Administrator/source/repos/Project Blackhole/Project Blackhole/bin/Source Pic/";
-        public static string Path = "C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python.exe";
+        // For testing *** ( // before EXECUTING)
+        string Path = "C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python.exe";
         //***
         List<string> Arr = new List<string>();
-        void Run()
+        bool[] SLT = new bool[1001];
+        void GetMatchList()
         {
             //Provide script path
-            string Pyname = "C:/Users/Administrator/source/repos/Project Blackhole/Project Blackhole/bin/Testing Python/testing.py";
+            string Pyname = Directory.GetCurrentDirectory()+"/../../../photo_email/album.py";
+            Pyname = Directory.GetCurrentDirectory() + "/../Testing_Python/testing.py";
             //Create Process Info
             ProcessStartInfo StartInfo = new ProcessStartInfo(Path);
-            StartInfo.FileName = Path;
             StartInfo.Arguments = $"\"{Pyname}\"";
             //Config
             StartInfo.UseShellExecute = false;
@@ -51,20 +51,58 @@ namespace Project_Blackhole
                 else Buffer += output[i];
             }
         }
+        void SendEmail(string list)
+        {
+            //Provide script path
+            string Pyname = Directory.GetCurrentDirectory() + "/../../../photo_email/mail/main.py";
+            MessageBox.Show(Pyname);
+            //Pyname= Directory.GetCurrentDirectory() + "/../../../photo_email/ui-csharp/bin/Testing_Python/testing.py";
+            //Create Process Info
+            ProcessStartInfo StartInfo = new ProcessStartInfo(Path,Pyname+ " " +list);
+            //Config
+            StartInfo.UseShellExecute = false;
+            StartInfo.CreateNoWindow = true;
+            StartInfo.RedirectStandardError = true;
+            StartInfo.RedirectStandardOutput = true;
+            //Execution
+            string output = "";
+            string err = "";
+            using (Process pro = Process.Start(StartInfo))
+            {
+                err = pro.StandardError.ReadToEnd();
+                output = pro.StandardOutput.ReadToEnd();
+            }
+            string Buffer = "";
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (output[i] == '\n')
+                {
+                    Arr.Add(Buffer);
+                    Buffer = "";
+                }
+                else Buffer += output[i];
+            }
+        }
+        void Ini()
+        {
+            for (int i = 0; i <= 1000; i++) SLT[i] = false;
+        }
         public Choose(string s)
         {
+            //Path = s;
             InitializeComponent();
-            Run();
+            GetMatchList();
+            Ini();
         }
         int cur = 0;
         void Setbrowse(int from)
         {
-            if (from < Arr.Count) pictureBox1.ImageLocation = Arr[from]; else pictureBox1.ImageLocation = st + "unavailable.png";
-            if (from+1 < Arr.Count) pictureBox2.ImageLocation = Arr[from+1]; else pictureBox2.ImageLocation = st + "unavailable.png";
-            if (from+2 < Arr.Count) pictureBox3.ImageLocation = Arr[from+2]; else pictureBox3.ImageLocation = st + "unavailable.png";
-            if (from+3 < Arr.Count) pictureBox4.ImageLocation = Arr[from+3]; else pictureBox4.ImageLocation = st + "unavailable.png";
-            if (from+4 < Arr.Count) pictureBox5.ImageLocation = Arr[from+4]; else pictureBox5.ImageLocation = st + "unavailable.png";
-            if (from+5 < Arr.Count) pictureBox6.ImageLocation = Arr[from+5]; else pictureBox6.ImageLocation = st + "unavailable.png";
+            if (from < Arr.Count) pictureBox1.ImageLocation = Arr[from]; else pictureBox1.ImageLocation = Directory.GetCurrentDirectory()+ "../SourcePic/unavailable.png";
+            if (from+1 < Arr.Count) pictureBox2.ImageLocation = Arr[from+1]; else pictureBox2.ImageLocation = Directory.GetCurrentDirectory() + "../SourcePic/unavailable.png";
+            if (from+2 < Arr.Count) pictureBox3.ImageLocation = Arr[from+2]; else pictureBox3.ImageLocation = Directory.GetCurrentDirectory() + "../SourcePic/unavailable.png";
+            if (from+3 < Arr.Count) pictureBox4.ImageLocation = Arr[from+3]; else pictureBox4.ImageLocation = Directory.GetCurrentDirectory() + "../SourcePic/unavailable.png";
+            if (from+4 < Arr.Count) pictureBox5.ImageLocation = Arr[from+4]; else pictureBox5.ImageLocation = Directory.GetCurrentDirectory() + "../SourcePic/unavailable.png";
+            if (from+5 < Arr.Count) pictureBox6.ImageLocation = Arr[from+5]; else pictureBox6.ImageLocation = Directory.GetCurrentDirectory() + "../SourcePic/unavailable.png";
         }
         private void Choose_Load(object sender, EventArgs e)
         {
@@ -73,37 +111,46 @@ namespace Project_Blackhole
             prev.Hide();
             if (Arr.Count <= 6) next.Hide();
         }
-
-        
-
         private void PictureBox1_Click(object sender, EventArgs e)
         {
-
+            if (cur * 6 >= Arr.Count) return;
+            SLT[cur * 6] ^= true;
+            pictureBox7.Visible = (SLT[cur*6]);
         }
 
         private void PictureBox2_Click(object sender, EventArgs e)
         {
-
+            if (cur * 6 + 1 >= Arr.Count) return;
+            SLT[cur * 6 + 1] ^= true;
+            pictureBox8.Visible = (SLT[cur * 6 + 1]);
         }
 
         private void PictureBox3_Click(object sender, EventArgs e)
         {
-
+            if (cur * 6 + 2 >= Arr.Count) return;
+            SLT[cur * 6 + 2] ^= true;
+            pictureBox9.Visible = (SLT[cur * 6 + 2]);
         }
 
         private void PictureBox4_Click(object sender, EventArgs e)
         {
-
+            if (cur * 6 + 3 >= Arr.Count) return;
+            SLT[cur * 6 + 3] ^= true;
+            pictureBox10.Visible = (SLT[cur * 6 + 3]);
         }
 
         private void PictureBox5_Click(object sender, EventArgs e)
-        {
-
+        { 
+            if (cur* 6 + 4 >= Arr.Count) return;
+            SLT[cur * 6 + 4] ^= true;
+            pictureBox11.Visible = (SLT[cur * 6 + 4]);
         }
 
         private void PictureBox6_Click(object sender, EventArgs e)
         {
-
+            if (cur * 6 + 5 >= Arr.Count) return;
+            SLT[cur * 6 + 5] ^= true;
+            pictureBox12.Visible = (SLT[cur * 6 + 5]);
         }
 
         private void Prev_Click(object sender, EventArgs e)
@@ -118,6 +165,24 @@ namespace Project_Blackhole
             prev.Show();
             if ((++cur+1)*6 >= Arr.Count) next.Hide();
             Setbrowse(cur * 6);
+        }
+
+        private void PictureBox7_Click(object sender, EventArgs e)
+        {
+            //ignore
+        }
+
+        private void Confirm_button_Click(object sender, EventArgs e)
+        {
+            string Arg_Py="";
+            for (int i = 0; i < Arr.Count; i++)
+            {
+                if (SLT[i]) Arg_Py += Arr[i] + " ";
+            }
+            //MessageBox.Show("The chosen files:\n" + Arg_Py, "Comfirmation");
+            SendEmail(Arg_Py);
+            MessageBox.Show("Email Sent!", "Notice");
+            this.Hide();
         }
     }
 }
