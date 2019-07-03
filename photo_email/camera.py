1908915b304c90d90818aeb4c5914b16b496ec40
4E14 Chu Yat Hong <sy9660@syss.edu.hk>
2019-07-03 14:25:21 +0800

    Modified camera.py

--- photo_email/camera.py
+++ photo_email/camera.py
@@ -4,11 +4,13 @@ import face_recognition as fr
 from face import face
 from album import album
 
-def take_photo_get_path_from(albumPath):
+def take_photo(albumPath):
     img = 0
     display = 0
 
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -17,7 +19,7 @@ def take_photo_get_path_from(albumPath):
         if len(location) > 0:
             for fc in location:
                 display = cv2.rectangle(display, (fc[3], fc[0]), (fc[1], fc[2]), (0,255,0), 5)
-
+        
         cv2.imshow('frame',display)
         key = cv2.waitKey(1)
 
@@ -33,7 +35,7 @@ def take_photo_get_path_from(albumPath):
 
     alb = album.load(albumPath)
 
-    for photo in alb.match(pht, 0.6):
-        print(photo[0].path)
+    # for photo in alb.match(pht, 0.6):
+    #     print(photo[0].path)
 
 take_photo_get_path_from(r'C:\FaceRecognition\photo-email\tests\test_data\test_data_data');
